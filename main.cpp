974d06d95d9d5b3b05f8860e562fc79989f3d10d
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-12-28 14:42:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,10 +34,22 @@ string to_inverted(string straight, int bit_size){
             straight[i] = '1';
         else
             straight[i] = '0';
-    return straight;
     }
+    return straight;
 }
 
+
+string to_additional(string inverted, int bit_size){
+    for(int i = bit_size; i > 0; i--){
+        if (inverted[i] == '0'){
+            inverted[i] = '1';
+            return inverted;
+        }
+        else
+        inverted[i] = '0';
+    }
+    return "Overflow happened: " + string(bit_size, '0');
+}
 
 int main()
 {
@@ -52,15 +64,13 @@ int main()
     if (flag)
         cout << to_straight(number, bit_size);
     else{
-        string straight;
+        string straight, inverted;
         bit_size -= 1;
-        straight = '1' + to_straight(number, bit_size);
-        cout << straight;
+        straight = '1' + to_straight(-number, bit_size);
+        cout << straight << endl;
         inverted = to_inverted(straight, bit_size);
-        cout << inverted;
-        cout << to_additional(inverted, bit_size)
-        
+        cout << inverted << endl;
+        cout << to_additional(inverted, bit_size);
+
     }
-
-
 }
